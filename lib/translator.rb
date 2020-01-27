086ea6b5d9e170763d024653ae11331647621e99
Valerie Foster <fosterv2@outlook.com>
2020-01-27 00:44:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,25 @@
-# require modules here
+require 'yaml'
 
 def load_library
-  # code goes here
+  emoticons = YAML.load_file('emoticons.yml')
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(eng_emoticon)
+  emoticons = load_library
+  emoticons.reduce({}) do |memo, (key, value)|
+    if value[0] == eng_emoticon
+      return value[1]
+    end
+    memo
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(jap_emoticon)
+  emoticons = load_library
+  emoticons.reduce({}) do |memo, (key, value)|
+    if value[1] == jap_emoticon
+      return key
+    end
+    memo
+  end
 end
